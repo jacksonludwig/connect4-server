097f0321767baf5d8b400fa9fa4f502935f4a1e5
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-01-17 21:44:28 -0500

    basic event loop

--- main.go
+++ main.go
@@ -8,11 +8,42 @@ import (
   "github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{}
+
+// TODO disallow wrong origin
+func checkRequestOrigin(req *http.Request) bool {
+  return true
+}
+
+func connect(writer http.ResponseWriter, req *http.Request) {
+  upgrader.CheckOrigin = checkRequestOrigin
+
+  connection, err := upgrader.Upgrade(writer, req, nil)
+
+  if err != nil {
+    fmt.Printf("error upgrading connection: %s\n", err.Error())
+    return
+  }
+
+  defer connection.Close()
+
+  // main event loop
+  for {
+    _, msg, err := connection.ReadMessage()
+
+    if err != nil {
+      fmt.Printf("error upgrading connection: %s\n", err.Error())
+      break
+    }
+
+    fmt.Printf("message received: %s\n", msg)
+  }
+}
+
 func main() {
   router := mux.NewRouter()
 
-  // TODO
-  router.HandleFunc("/createGame", createGame)
+  router.HandleFunc("/connect", connect)
 
   log.Fatal(http.ListenAndServe(":9100", router))
 }
